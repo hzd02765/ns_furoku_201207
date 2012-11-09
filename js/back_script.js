2bcafcfd138ab2a59a0894eeb35845a3782f8d98
tomoyuki@Ubuntu <hzd02765@nifty.com>
2012-11-09 13:49:47 +0900

    page 041

---
+++ js/back_script.js
@@ -0,0 +1,5 @@
+$(document).bind('mobileinit', function(){
+    $.mobile.page.prototype.options.addBackBtn = true;
+    $.mobile.page.prototype.options.backBtnText = '戻る';
+    $.mobile.page.prototype.options.backBtnTheme = 'b';
+});
