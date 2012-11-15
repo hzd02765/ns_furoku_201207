95d5e0ec95754847b7e71159f4604b982c6729b5
SakaiTomoyuki@MacBook <hzd02765@nifty.com>
2012-11-15 12:13:41 +0900

    page 060

---
+++ js/dialog_custom.js
@@ -0,0 +1,9 @@
+$(docment).bind('mobileinit', function(){
+    $.mobile.dialog.prototype.options.closeBtnText = '閉じる';
+});
+
+$('#linkpg').live('pageinit', function(){
+    $('#linkpg_ok').click(function(){
+	$('.ui-dialog').dialog('close');
+    });
+});
---
+++ js/loading.js
@@ -0,0 +1,5 @@
+$(docment).bind('mobileinit', funtion(){
+    $.mobile.loadingMessageTextVisible = true;
+    $.mobile.loadingMessage = '読み込み中...';
+    $.mobile.pageLoadErrorMessage = '読み込みエラーです';
+});
